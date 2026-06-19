62dd0dc9ff42243a24db8bc9fc68e8482843a98c
sholt6 <samueljrholt@gmail.com>
2026-06-19 16:30:07 +0100

    Added argparser, minor fixes

--- scripts/mapq_filter_reads.py
+++ scripts/mapq_filter_reads.py
@@ -1,7 +1,25 @@
 #!/usr/bin/env python
 
-import sys
 import csv
+import argparse
+
+parser = argparse.ArgumentParser(
+    prog="mapq_filter_reads.py",
+    description="""Accepts a BED file containing break sites identified by
+    INDUCE-Seq along with metadata. Filters on quality column and outputs to a
+    new BED file.""",
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+parser.add_argument('input_bed',
+                    help="""path to an INDUCE-seq breaks BED""")
+
+parser.add_argument('filtered_bed',
+                    help="""desired path to output filtered BED""")
+
+parser.add_argument('mapq_threshold', type=int, default=30,
+                    help="""path to an INDUCE-seq breaks BED""")
+
 
 def bed_reader(file_name):
     with open(file_name, "r") as file:
@@ -12,22 +30,25 @@ def bed_reader(file_name):
 
 def filter_by_score(rows, min_score=30):
     for row in rows:
-        if int(row[4]) >= 30:
+        if int(row[4]) >= min_score:
             yield row
 
 
 def main():
-    input_bed = sys.argv[1]
-    output_bed = sys.argv[2]
-    mapq_threshold = sys.argv[3]
+    args = parser.parse_args()
+
+    input_bed = args.input_bed
+    filtered_bed = args.filtered_bed
+    mapq_threshold = args.mapq_threshold
 
     read_bed = bed_reader(input_bed)
     filtered_bed_rows = filter_by_score(read_bed, mapq_threshold)
     
-    with open('filtered.bed', "w", newline="") as outfile:
+    with open(filtered_bed, "w", newline="") as outfile:
         writer = csv.writer(outfile, delimiter="\t", lineterminator="\n")
         for row in filtered_bed_rows:
             writer.writerow(row)
+
 
 if __name__ == "__main__":
     main()
